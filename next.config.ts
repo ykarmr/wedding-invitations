7e7a248fc0e416438b9f62e10b232c91c22f5cd5
ykarmr <yuki.arimura0508@gmail.com>
2026-06-16 20:22:42 +0900

    微修正

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: process.env.NODE_ENV === 'production' ? "/family-greeting" : undefined,
+  basePath: process.env.NODE_ENV === 'production' ? "/wedding-invitations" : undefined,
   reactCompiler: true,
 };
 
