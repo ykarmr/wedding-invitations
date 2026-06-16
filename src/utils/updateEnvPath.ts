65e5ca94135b07263ec83d99cca68c15cea09371
ykarmr <yuki.arimura0508@gmail.com>
2026-06-16 19:44:59 +0900

    実装の修正

--- src/utils/updateEnvPath.ts
+++ src/utils/updateEnvPath.ts
@@ -1,6 +1,6 @@
 const updateEnvPath = (filename: string) => {
   const isProd = process.env.NODE_ENV === 'production';
-  const prefixPath = isProd ? '/family-greeting' : '';
+  const prefixPath = isProd ? '/wedding-invitations' : '';
 
   return prefixPath + filename;
 };
